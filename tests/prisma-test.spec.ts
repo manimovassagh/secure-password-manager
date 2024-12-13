1ab51119520f08ce57f4f2a84d6ba1fcc16327ec
Mani Movassagh <mani.mgh@gmail.com>
2024-12-13 21:13:31 +0100

    test

--- tests/prisma-test.spec.ts
+++ tests/prisma-test.spec.ts
@@ -5,14 +5,14 @@ const prisma = new PrismaClient();
 test("Should create a new user", async () => {
   const newUser = await prisma.user.create({
     data: {
-      username: "jest_user",
-      email: "jest_user@example.com",
+      username: "jest_user233",
+      email: "jest_user233@example.com",
       hashedMasterPassword: "hashed_password_example",
     },
   });
 
   expect(newUser).toHaveProperty("id");
-  expect(newUser.username).toBe("jest_user");
+  expect(newUser.username).toBe("jest_user233");
 
   // Clean up after test
   await prisma.user.delete({ where: { id: newUser.id } });
